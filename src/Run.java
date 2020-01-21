34a2d93d2b409f39bf38357912b7cbd018dc3cef
Conor-Behard333 <47724661+Conor-Behard333@users.noreply.github.com>
2020-01-21 14:23:10 +0000

    searches for checkmate once a player has moved

--- src/Run.java
+++ src/Run.java
@@ -18,6 +18,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Run extends Application {
+    // TODO: 17/01/2020 Add castling and pawn swap and en passant
     private ArrayList<double[]> moves = new ArrayList<>();
     private ArrayList<double[]> tempMoves = new ArrayList<>();
     private boolean p1;
@@ -30,26 +31,26 @@ public class Run extends Application {
     private Label playerTurn;
     private boolean check = false;
     private boolean notInCheck = true;
-    Stage stage;
-    
-    
+    private Stage stage;
+
+
     public static void main(String[] args) {
         launch();
     }
-    
+
     public void start(Stage stage) throws Exception {
         p1 = true;
         playerTurn = new Label("White Players turn");
         playerTurn.setFont(new Font(50));
         playerTurn.setTranslateX(210);
         playerTurn.setTranslateY(825);
-        
+
         Group board = createBoard();
         ImageView[] imageViews = addPieceIcons();
-        
+
         Line border = getBorder();
         board.getChildren().addAll(imageViews);
-        
+
         Group window = new Group();
         window.getChildren().addAll(board, border, playerTurn);
         Scene scene = new Scene(window, 790, 900);
@@ -58,7 +59,7 @@ public class Run extends Application {
         stage.show();
         this.stage = stage;
     }
-    
+
     private Line getBorder() {
         Line border = new Line();
         border.setStartX(0);
@@ -68,12 +69,12 @@ public class Run extends Application {
         border.setStrokeWidth(5);
         return border;
     }
-    
+
     private ImageView[] addPieceIcons() throws FileNotFoundException {
         FileInputStream[] images = new FileInputStream[32];
         File[] folder = {new File("Piece Icons\\Black"), new File("Piece Icons\\White")};
         File[][] files = {folder[0].listFiles(), folder[1].listFiles()};
-        
+
         for (int i = 0; i < files[0].length + files[1].length; i++) {
             if (i < 16) {
                 images[i] = new FileInputStream("Piece Icons\\Black\\" + files[0][i].getName());
@@ -81,7 +82,7 @@ public class Run extends Application {
                 images[i] = new FileInputStream("Piece Icons\\White\\" + files[1][i - 16].getName());
             }
         }
-        
+
         int x = 0;
         int y = 0;
         boolean whitePlayer = true;
@@ -92,13 +93,13 @@ public class Run extends Application {
             pieces[i].setFitWidth(95);
             pieces[i].setX(x);
             pieces[i].setY(y);
-            
+
             if (i < 16) {
                 pieces[i].setId(findPattern(files[0][i].getName(), "\\d_[A-Z][a-z]+_[a-zA-Z]+_\\d", 0, 0));
             } else {
                 pieces[i].setId(findPattern(files[1][i - 16].getName(), "\\d_[A-Z][a-z]+_[a-zA-Z]+_\\d", 0, 0));
             }
-            
+
             x += 100;
             if ((i + 1) % 8 == 0 && i > 8 && whitePlayer) {
                 y = 600;
@@ -111,14 +112,14 @@ public class Run extends Application {
         }
         return pieces;
     }
-    
-    public Group createBoard() throws Exception {
+
+    private Group createBoard() throws Exception {
         Group temp = new Group();
         int x = 0;
         int y = 0;
         boolean whiteTile = true;
         ImageView[][] board = new ImageView[8][8];
-        
+
         for (int row = 0; row < board.length; row++) {
             for (int col = 0; col < board[row].length; col++) {
                 if (whiteTile) {
@@ -149,7 +150,7 @@ public class Run extends Application {
         }
         return temp;
     }
-    
+
     private void playTurn(ImageView[][] board, int row, int col, String colour) {
         highlightMoves(board, true, colour);
         if (board[row][col].getId().contains(colour) || movePiece) {
@@ -161,7 +162,7 @@ public class Run extends Application {
                 String newId = board[row][col].getId();
                 String opponentColour = getOpponentColour(colour);
                 boolean pawn = id.contains("Pawn") && newId.contains("Pawn");
-                
+
                 if ((newId.equalsIgnoreCase("empty") || newId.contains(opponentColour)) && validMove(newX, newY) && !pawn) {
                     movePiece(board, board[row][col], colour, newX, newY, newId.contains(opponentColour));
                 } else if (validMove(newX, newY)) {
@@ -172,24 +173,23 @@ public class Run extends Application {
             }
         }
     }
-    
+
     private void pickPiece(ImageView[][] board, String colour, ImageView piece) {
         moves.clear();
-        searchForCheckMate(board, colour, piece);
         addPieceMoves(piece, colour, board, true);
         highlightMoves(board, false, colour);
     }
-    
-    private void searchForCheckMate(ImageView[][] board, String colour, ImageView piece) {
+
+    private void searchForCheckMate(ImageView[][] board, String colour) {
         moves.clear();
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (board[j][i].getId().contains(colour) && !board[j][i].getId().equalsIgnoreCase(piece.getId())) {
+                if (board[j][i].getId().contains(colour)) {
                     addPieceMoves(board[j][i], colour, board, true);
                 }
             }
         }
-        addPieceMoves(piece, colour, board, true);
+        System.out.println(moves.size());
         if (moves.size() == 0) {
             //reset
             String player;
@@ -198,10 +198,10 @@ public class Run extends Application {
             } else {
                 player = "White";
             }
-            
+
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION, player + " Won");
             alert.showAndWait();
-            
+
             try {
                 stage.close();
                 start(new Stage());
@@ -211,7 +211,7 @@ public class Run extends Application {
         }
         moves.clear();
     }
-    
+
     private void highlightMoves(ImageView[][] board, boolean remove, String colour) {
         ColorAdjust colorAdjust = new ColorAdjust();
         colorAdjust.setContrast(0.4);
@@ -222,7 +222,7 @@ public class Run extends Application {
             highlight(board, remove, colour, colorAdjust, move);
         }
     }
-    
+
     private void highlight(ImageView[][] board, boolean remove, String colour, ColorAdjust colorAdjust, double[] move) {
         if (remove) {
             board[(int) move[0] / 100][(int) move[1] / 100].setEffect(null);
@@ -230,7 +230,7 @@ public class Run extends Application {
             board[(int) move[0] / 100][(int) move[1] / 100].setEffect(colorAdjust);
         }
     }
-    
+
     private void movePiece(ImageView[][] board, ImageView clickedSquare, String colour, double newX, double newY, boolean opponent) {
         int index = findPiece(id);//get the index of the piece clicked
         pieces[index].setX(newX);//move it to the new coordinates
@@ -244,13 +244,14 @@ public class Run extends Application {
         p1 = colour.equalsIgnoreCase("black");
         pickPiece = true;
         movePiece = false;
+        searchForCheckMate(board, getOpponentColour(colour));
         if (p1) {
             playerTurn.setText("White Players Turn");
         } else {
             playerTurn.setText("Black Players Turn");
         }
     }
-    
+
     private void addPieceMoves(ImageView piece, String colour, ImageView[][] board, boolean actualMove) {
         String id = piece.getId();
         double x = piece.getX();
@@ -287,28 +288,28 @@ public class Run extends Application {
         pickPiece = false;
         movePiece = true;
     }
-    
+
     private void kingMoves(double x, double y, ImageView[][] board, String opponentColour, ImageView piece, boolean actualMove) {
         double[] newX = new double[]{x + 100, x - 100, x + 100, x - 100, x + 100, x - 100, x, x};
         double[] newY = new double[]{y + 100, y + 100, y - 100, y - 100, y, y, y + 100, y - 100};
-    
+
         for (int i = 0; i < 8; i++) {
             addConsecutiveMoves(newX[i], newY[i], board, getOpponentColour(opponentColour), piece, actualMove, 0, 0, true);
         }
     }
-    
+
     private void bishopMoves(double x, double y, ImageView[][] board, String colour, ImageView piece, boolean actualMove) {
         double yUp = y - 100;
         double yDown = y + 100;
         double xRight = x + 100;
         double xLeft = x - 100;
-    
+
         addConsecutiveMoves(xRight, yUp, board, colour, piece, actualMove, 100, -100, false);//upper right
         addConsecutiveMoves(xRight, yDown, board, colour, piece, actualMove, 100, 100, false);//bottom right
         addConsecutiveMoves(xLeft, yUp, board, colour, piece, actualMove, -100, -100, false);//upper left
         addConsecutiveMoves(xLeft, yDown, board, colour, piece, actualMove, -100, 100, false);//bottom left
     }
-    
+
     private void addConsecutiveMoves(double newX, double newY, ImageView[][] board, String colour, ImageView piece, boolean actualMove, int xIncrement, int yIncrement, boolean singleMove) {
         double x = newX;
         double y = newY;
@@ -325,19 +326,19 @@ public class Run extends Application {
             y += yIncrement;
         }
     }
-    
+
     private void rookMoves(double x, double y, ImageView[][] board, String colour, ImageView piece, boolean actualMove) {
         addConsecutiveMoves(x + 100, y, board, colour, piece, actualMove, 100, 0, false);//all right
         addConsecutiveMoves(x - 100, y, board, colour, piece, actualMove, -100, 0, false);//all left
         addConsecutiveMoves(x, y + 100, board, colour, piece, actualMove, 0, 100, false);//all up/down
         addConsecutiveMoves(x, y - 100, board, colour, piece, actualMove, 0, -100, false);//all down/up
     }
-    
+
     private boolean isEmpty(double x, double y, ImageView[][] board) {
         String id = board[(int) (x / 100)][(int) (y / 100)].getId();
         return id.equalsIgnoreCase("empty");
     }
-    
+
     private void knightMoves(ImageView[][] board, String opponentColour, double x, double y, ImageView piece, boolean actualMove) {
         double[] newX = new double[]{x + 100, x + 100, x - 100, x - 100, x + 200, x + 200, x - 200, x - 200};
         double[] newY = new double[]{y + 200, y - 200, y + 200, y - 200, y + 100, y - 100, y + 100, y - 100};
@@ -345,11 +346,11 @@ public class Run extends Application {
             addConsecutiveMoves(newX[i], newY[i], board, getOpponentColour(opponentColour), piece, actualMove, 0, 0, true);
         }
     }
-    
+
     private boolean checkBounds(double x, double y) {
         return x >= 0 && x <= 700 && y >= 0 && y <= 700;
     }
-    
+
     private boolean validMove(double newX, double newY) {
         for (double[] move : moves) {
             if (newX == move[0] && newY == move[1]) {
@@ -358,7 +359,7 @@ public class Run extends Application {
         }
         return false;
     }
-    
+
     private void pawnMoves(double x, double y, ImageView[][] board, String colour, ImageView piece, boolean actualMove) {
         if (colour.equalsIgnoreCase("White")) {
             addPawnMoves(x, y, board, "Black", y - 100, piece, actualMove);
@@ -366,7 +367,7 @@ public class Run extends Application {
             addPawnMoves(x, y, board, "White", y + 100, piece, actualMove);
         }
     }
-    
+
     private void addPawnMoves(double x, double y, ImageView[][] board, String opponentColour, double direction, ImageView piece, boolean actualMove) {
         if (opponentColour.equalsIgnoreCase("Black") && y == 600.0 && board[(int) x / 100][(int) (y - 100) / 100].getId().equalsIgnoreCase("empty") && board[(int) x / 100][(int) (y - 200) / 100].getId().equalsIgnoreCase("empty")) {
             addMove(board, opponentColour, x, y - 200, piece, actualMove);//move white up 2 squares
@@ -387,7 +388,7 @@ public class Run extends Application {
             }
         }
     }
-    
+
     private void addMove(ImageView[][] board, String opponentColour, double x, double y, ImageView piece, boolean actualMove) {
         if (check) {
             if (moveGetsOutOfCheck(board, opponentColour, new double[]{x, y}, piece)) {
@@ -404,7 +405,7 @@ public class Run extends Application {
             }
         }
     }
-    
+
     private void addMove(double x, double y, boolean actualMove) {
         if (actualMove) {
             moves.add(new double[]{x, y});//look at left diagonal
@@ -412,13 +413,13 @@ public class Run extends Application {
             tempMoves.add(new double[]{x, y});
         }
     }
-    
+
     private boolean moveGetsInCheck(ImageView[][] board, String opponentColour, double[] move, ImageView piece) {
         int row = (int) piece.getX() / 100;
         int col = (int) piece.getY() / 100;
         int moveX = (int) move[0] / 100;
         int moveY = (int) move[1] / 100;
-        
+
         String id = board[row][col].getId();
         String moveId = board[moveX][moveY].getId();
         board[row][col].setId("empty");
@@ -435,19 +436,19 @@ public class Run extends Application {
         notInCheck = true;
         return false;
     }
-    
-    
+
+
     private boolean moveGetsOutOfCheck(ImageView[][] board, String opponentColour, double[] move, ImageView piece) {
         int moveX = (int) move[0] / 100;
         int moveY = (int) move[1] / 100;
         int row = (int) piece.getX() / 100;
         int col = (int) piece.getY() / 100;
-        
+
         String id = board[row][col].getId();
         String moveId = board[moveX][moveY].getId();
         board[row][col].setId("empty");
         board[moveX][moveY].setId(id);
-        
+
         check = false;
         if (searchForCheck(opponentColour, board)) {
             check = true;
@@ -457,13 +458,13 @@ public class Run extends Application {
             check = true;
             return true;
         }
-        
+
         board[row][col].setId(id);
         board[moveX][moveY].setId(moveId);
         return false;
     }
-    
-    
+
+
     private boolean searchForCheck(String colour, ImageView[][] board) {
         tempMoves.clear();
         for (int i = 0; i < 8; i++) {
@@ -473,7 +474,7 @@ public class Run extends Application {
                 }
             }
         }
-        
+
         for (double[] move : tempMoves) {
             if (board[(int) move[0] / 100][(int) move[1] / 100].getId().contains(getOpponentColour(colour) + "_King")) {
                 return true;
@@ -481,7 +482,7 @@ public class Run extends Application {
         }
         return false;
     }
-    
+
     private int findPiece(String id) {
         for (int i = 0; i < pieces.length; i++) {
             if (pieces[i].getId().equalsIgnoreCase(id)) {
@@ -490,7 +491,7 @@ public class Run extends Application {
         }
         return 0;
     }
-    
+
     private String getDescription(int row, int col) {
         if ((row == 0 || row == 7) && (col == 0 || col == 7)) {
             return row + getColour(row) + "_Rook_" + col;
@@ -507,11 +508,11 @@ public class Run extends Application {
         }
         return "empty";
     }
-    
+
     private String getColour(int row) {
         return (row == 0 || row == 1) ? "_Black" : "_White";
     }
-    
+
     private String findPattern(String string, String regex, int start, int end) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(string);
@@ -520,7 +521,7 @@ public class Run extends Application {
         }
         return string;
     }
-    
+
     private String getOpponentColour(String colour) {
         if (colour.equalsIgnoreCase("white")) {
             return "Black";
